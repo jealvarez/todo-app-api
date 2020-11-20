ae47b6c212591c33d44fa1c5eaa594b676e23dff
Jorge Alvarez <alvarez.jeap@gmail.com>
2020-11-19 20:50:10 -0600

    [master] Fix database init script

--- scripts/bash/init-database.sh
+++ scripts/bash/init-database.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd)/scripts:/scripts mysql:8 mysql -h 192.168.99.1 -u root -pr00t -P 3306 < scripts/sql/init-database.sql
+mysql -h 192.168.99.1 -u root -pr00t -P 3306 < scripts/sql/init-database.sql
